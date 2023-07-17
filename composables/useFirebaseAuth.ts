7dfec5c959dedcd56012c9bff71430864afe1ec4
yuto <yuto.0313king@icloud.com>
2023-07-18 01:49:13 +0900

    ✨ firebaseによるEmail&passwordのサインイン機能追加

--- composables/useFirebaseAuth.ts
+++ composables/useFirebaseAuth.ts
@@ -1,4 +1,8 @@
-import { createUserWithEmailAndPassword, User } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  User,
+} from "firebase/auth";
 
 export default function () {
   const { $auth } = useNuxtApp();
@@ -21,7 +25,26 @@ export default function () {
       }
     } catch (error: unknown) {
       if (error instanceof Error) {
-        // handle error
+        console.error(error);
+      }
+      return false;
+    }
+    return false;
+  };
+
+  const signIn = async (email: string, password: string): Promise<boolean> => {
+    try {
+      const userCreds = await signInWithEmailAndPassword(
+        $auth,
+        email,
+        password
+      );
+      if (userCreds) {
+        user.value = userCreds.user;
+        return true;
+      }
+    } catch (error: unknown) {
+      if (error instanceof Error) {
         console.error(error);
       }
       return false;
@@ -32,5 +55,6 @@ export default function () {
   return {
     user,
     registerUser,
+    signIn,
   };
 }
