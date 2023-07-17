e4eeeb89346bf59de03ecd7bb8ead6afdd0fb0bf
yuto <yuto.0313king@icloud.com>
2023-07-17 13:41:57 +0900

    ✨ firebaseによるemail&password認証(signup)

---
+++ composables/useFirebaseAuth.ts
@@ -0,0 +1,36 @@
+import { createUserWithEmailAndPassword, User } from "firebase/auth";
+
+export default function () {
+  const { $auth } = useNuxtApp();
+
+  const user = useState<User | null>("fb_user", () => null);
+
+  const registerUser = async (
+    email: string,
+    password: string
+  ): Promise<boolean> => {
+    try {
+      const userCreds = await createUserWithEmailAndPassword(
+        $auth,
+        email,
+        password
+      );
+      if (userCreds) {
+        user.value = userCreds.user;
+        return true;
+      }
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        // handle error
+        console.error(error);
+      }
+      return false;
+    }
+    return false;
+  };
+
+  return {
+    user,
+    registerUser,
+  };
+}
