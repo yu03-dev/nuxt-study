a16958a5e153def9ae951c41c002117eb6c9f840
yuto <yuto.0313king@icloud.com>
2023-07-17 13:43:03 +0900

    💄 signup画面を作成

--- components/Form.vue
+++ components/Form.vue
@@ -1,15 +1,5 @@
 <template>
-  <form>
-    <v-text-field
-      v-model="state.name"
-      :error-messages="v$.name.$errors.map((e) => e.$message)"
-      :counter="10"
-      label="Name"
-      required
-      @input="v$.name.$touch"
-      @blur="v$.name.$touch"
-    ></v-text-field>
-
+  <form class="form">
     <v-text-field
       v-model="state.email"
       :error-messages="v$.email.$errors.map((e) => e.$message)"
@@ -19,19 +9,34 @@
       @blur="v$.email.$touch"
     ></v-text-field>
 
-    <v-btn class="me-4" @click="v$.$validate"> submit </v-btn>
-    <v-btn @click="clear"> clear </v-btn>
+    <v-text-field
+      v-model="state.password"
+      :error-messages="v$.password.$errors.map((e) => e.$message)"
+      :counter="14"
+      type="password"
+      label="Password"
+      required
+      @input="v$.password.$touch"
+      @blur="v$.password.$touch"
+    ></v-text-field>
+
+    <div class="form-btn">
+      <v-btn class="me-4" @click="handleSubmit"> submit </v-btn>
+      <v-btn @click="clear"> clear </v-btn>
+    </div>
   </form>
 </template>
 
 <script setup>
 import { reactive } from "vue";
 import { useVuelidate } from "@vuelidate/core";
-import { email, required } from "@vuelidate/validators";
+import { email, required, minLength } from "@vuelidate/validators";
+
+const { registerUser } = useFirebaseAuth();
 
 const initialState = {
-  name: "",
   email: "",
+  password: "",
 };
 
 const state = reactive({
@@ -39,17 +44,42 @@ const state = reactive({
 });
 
 const rules = {
-  name: { required },
   email: { required, email },
+  password: { required },
 };
 
 const v$ = useVuelidate(rules, state);
 
-function clear() {
+const handleSubmit = async () => {
+  if (await !v$.value.$validate) {
+    alert("入力内容に誤りがあります");
+    return;
+  }
+  if (await registerUser(state.email, state.password)) {
+    alert("ログインに成功しました!!!");
+    clear();
+  } else {
+    alert("ログインに失敗しました");
+  }
+};
+
+const clear = () => {
   v$.value.$reset();
 
   for (const [key, value] of Object.entries(initialState)) {
     state[key] = value;
   }
-}
+};
 </script>
+
+<style scoped>
+.form {
+  width: 50%;
+  margin: 0 auto;
+}
+.form-btn {
+  display: flex;
+  justify-content: center;
+  margin-top: 1rem;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,26 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// const runtimeConfig = useRuntimeConfig();
+
+// console.log(runtimeConfig);
+</script>
 
 <template>
-  <Form />
+  <div class="container">
+    <h1>Hello Nuxt.js!!</h1>
+    <p>Let's sign up</p>
+    <Form />
+  </div>
 </template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.container h1,
+p {
+  margin: 1rem 0;
+}
+</style>
