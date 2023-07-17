c6a395caaaf55c74618a6f9e517aa769ab2675f1
yuto <yuto.0313king@icloud.com>
2023-07-18 01:50:12 +0900

    ♻️ provideで型補完可能に

--- plugins/firebase.client.ts
+++ plugins/firebase.client.ts
@@ -12,9 +12,10 @@ export default defineNuxtPlugin((nuxtApp) => {
   const auth = getAuth(app);
   const firestore = getFirestore(app);
 
-  nuxtApp.vueApp.provide("auth", auth);
-  nuxtApp.provide("auth", auth);
-
-  nuxtApp.vueApp.provide("firestore", firestore);
-  nuxtApp.provide("firestore", firestore);
+  return {
+    provide: {
+      auth: auth,
+      firestore: firestore,
+    },
+  };
 });
