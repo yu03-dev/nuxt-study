f8c7ff6fb84f9366b078951a3cb47cb5efee7206
yuto <yuto.0313king@icloud.com>
2023-07-20 22:18:34 +0900

    ✨ ユーザの情報を表示する画面を作成

---
+++ components/UserInfo.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>ユーザーの情報</h1>
+    <p>{{ user }}</p>
+    <VBtn @click="handleLogOut"> ログアウト </VBtn>
+  </div>
+</template>
+<script setup lang="ts">
+const { user, setUser, logOut } = useFirebaseAuth();
+
+onMounted(() => {
+  setUser();
+});
+
+const handleLogOut = async () => {
+  try {
+    if (await logOut()) {
+      alert("ログアウトしました");
+      return navigateTo("/");
+    }
+  } catch (error) {
+    console.error(error);
+  }
+  alert("ログアウトできませんでした。");
+};
+</script>
